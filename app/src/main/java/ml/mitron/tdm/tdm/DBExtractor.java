69b78b8a5ea0338d7ee310f1d08490b19c53024e
ACMCMC <ACMCMC@users.noreply.github.com>
2018-07-15 20:51:38 +0200

    Eliminadas librerías innecesarias

--- app/src/main/java/ml/mitron/tdm/tdm/DBExtractor.java
+++ app/src/main/java/ml/mitron/tdm/tdm/DBExtractor.java
@@ -2,7 +2,6 @@ package ml.mitron.tdm.tdm;
 
 import android.content.Context;
 import android.database.Cursor;
-import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 
