e0558147f6a0cf1c6cbfd76d12655c8a988cee28
ACMCMC <ACMCMC@users.noreply.github.com>
2018-08-14 05:18:23 +0200

    Versión inicial del Activity de la estación

--- app/src/main/java/ml/mitron/tdm/tdm/EstacionActivity.java
+++ app/src/main/java/ml/mitron/tdm/tdm/EstacionActivity.java
@@ -1,14 +1,19 @@
 package ml.mitron.tdm.tdm;
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import static ml.mitron.tdm.tdm.R.string.C;
 
 public class EstacionActivity extends AppCompatActivity {
 
@@ -35,14 +40,34 @@ public class EstacionActivity extends AppCompatActivity {
         for (Conexion conexion : estacion.getConexiones()) {
             LinearLayout layoutConexion = new LinearLayout(this);
             layoutConexion.setOrientation(LinearLayout.HORIZONTAL);
+            LinearLayout.LayoutParams parametros = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+            layoutConexion.setPadding(
+                    0,
+                    (int) getResources().getDimension(R.dimen.marginLineaVertical),
+                    0,
+                    (int) getResources().getDimension(R.dimen.marginLineaVertical));
+            layoutConexion.setLayoutParams(parametros);
 
             for (String linea : conexion.getLineas()) {
                 TextView idLinea = new TextView(this);
 
-                ViewGroup.LayoutParams parametros = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT);
+                parametros = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                //parametros.width = (int) getResources().getDimension(R.dimen.paddingLinea);
+                parametros.setMargins(
+                        (int) getResources().getDimension(R.dimen.marginLineaHorizontal),
+                        0,
+                        (int) getResources().getDimension(R.dimen.marginLineaHorizontal),
+                        0);
 
                 idLinea.setLayoutParams(parametros);
+                idLinea.setPadding(
+                        (int) getResources().getDimension(R.dimen.paddingLineaHorizontal),
+                        (int) getResources().getDimension(R.dimen.paddingLineaVertical),
+                        (int) getResources().getDimension(R.dimen.paddingLineaHorizontal),
+                        (int) getResources().getDimension(R.dimen.paddingLineaVertical));
+
                 idLinea.setBackgroundColor(Color.parseColor("#000000"));
+                idLinea.setTextColor(Color.parseColor("#FFFFFF"));
 
                 idLinea.setText((CharSequence) linea);
 
@@ -52,6 +77,11 @@ public class EstacionActivity extends AppCompatActivity {
                 layoutConexion.addView(idLinea);
             }
 
+            TextView idConexion = new TextView(this);
+            idConexion.setText(getResources().getText(R.string.conexionCon) + " " + extractor.getEstacion(conexion.getIDDestino()).getNombre());
+
+            layoutConexion.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            layoutConexion.addView(idConexion);
 
             contenedor.addView(layoutConexion);
         }
