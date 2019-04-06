52cc223e521ca9990eb4fe2a86d53e7b5eb5077f
acmc <20495460+ACMCMC@users.noreply.github.com>
2019-04-06 19:36:23 +0200

    Implementado un ViewPager para seleccionar la tarjeta en el proceso de compra

--- app/src/main/java/ml/mitron/tdm/CardBuySelectFragment.java
+++ app/src/main/java/ml/mitron/tdm/CardBuySelectFragment.java
@@ -2,18 +2,20 @@ package ml.mitron.tdm;
 
 
 import android.os.Bundle;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class CardBuySelectFragment extends Fragment {
+public class CardBuySelectFragment extends androidx.fragment.app.Fragment {
 
 
     public CardBuySelectFragment() {
@@ -25,7 +27,35 @@ public class CardBuySelectFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_card_buy_select, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_card_buy_select, container, false);
+
+        ((ViewPager) view.findViewById(R.id.viewPager_buy_select)).setAdapter(new ViewPagerAdapter(getFragmentManager()));
+
+        return view;
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        ViewPagerAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return (new ElementCardBuySelectFragment());
+                case 1:
+                    return (new InfinityCardBuySelectFragment());
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
     }
 
 }
